6a3698c7d71adb1f9ec2b0926feafa5e130cdfa8
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2020-09-12 21:05:30 +0530

    test(metrics): simplify time simulation

--- test/metrics.ts
+++ test/metrics.ts
@@ -5,33 +5,39 @@ import { Metrics } from "../src/metrics";
 
 describe("Metrics", () => {
   // simulate a monotonically increasing external timer
-  const times = [1, 4, 5, 6, 20, 20, 20, 25, 26];
-  const getCurrentTime = () => times.shift()!;
+  let time = 1;
+  const getCurrentTime = () => time;
 
   it("should be tracked correctly", () => {
-    // the numbers in the comments below indicate the value returned by
-    // `getCurrentTime` at that point
     const metrics = new Metrics(3, getCurrentTime);
 
     assert.equal(metrics.sum("foo"), undefined);
 
-    metrics.record("foo", 5); // 1
-    metrics.record("bar", 10); // 4
-    metrics.record("foo", 20); // 5
+    metrics.record("foo", 5);
+    time = 4;
+    metrics.record("bar", 10);
+    time = 5;
+    metrics.record("foo", 20);
 
     assert.equal(metrics.sum("foo"), 25);
-    metrics.prune(); // 6
+    time = 6;
+    metrics.prune();
     assert.equal(metrics.sum("foo"), 20);
     assert.equal(metrics.sum("bar"), 10);
-    metrics.prune(); // 20
+    time = 20;
+    metrics.prune();
     assert.equal(metrics.sum("foo"), 0);
     assert.equal(metrics.sum("bar"), 0);
-    metrics.record("baz", 10999); // 20
-    metrics.record("baz", 2); // 20
+    metrics.record("baz", 10999);
+    metrics.record("baz", 2);
     assert.equal(metrics.sum("baz"), 11001);
-    metrics.record("foo", 3); // 25
+
+    time = 25;
+    metrics.record("foo", 3);
     assert.equal(metrics.sum("foo"), 3);
-    metrics.prune(); // 26
+
+    time = 26;
+    metrics.prune();
     assert.equal(metrics.sum("baz"), 0);
     assert.equal(metrics.sum("foo"), 3);
   });
