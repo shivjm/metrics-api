a059769f26890b5c868959ed0ed055a697450632
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2020-09-12 23:04:52 +0530

    feat(run): read config from environment and print startup message

--- src/run.ts
+++ src/run.ts
@@ -1,17 +1,38 @@
-import { Level, Logger } from "./logger";
+import { Level, Logger, parseLevel } from "./logger";
 import { create } from "./server";
 import { getMonotonicTimeInSeconds } from "./time";
 
-// TODO read from environment
-const PORT = 3000;
-const MAX_AGE_IN_SECONDS = 60;
-const LOG_LEVEL = Level.TRACE;
-const PRUNE_INTERVAL_SECONDS = 1;
+// we could read these from a `.env` file but to keep things simple we'll
+// specify defaults here
+
+const {
+  METRICS_APP_PORT,
+  METRICS_APP_MAX_AGE_SECONDS,
+  METRICS_APP_PRUNE_INTERVAL_SECONDS,
+  METRICS_APP_LOG_LEVEL,
+} = {
+  METRICS_APP_PORT: "3000",
+  METRICS_APP_MAX_AGE_SECONDS: "60",
+  METRICS_APP_PRUNE_INTERVAL_SECONDS: "1",
+  METRICS_APP_LOG_LEVEL: "DEBUG",
+  ...process.env,
+};
+
+const logLevel = parseLevel(METRICS_APP_LOG_LEVEL);
+
+const logger = new Logger(logLevel);
+logger.info("Starting...", {
+  port: METRICS_APP_PORT,
+  maxAge: METRICS_APP_MAX_AGE_SECONDS,
+  pruneInterval: METRICS_APP_PRUNE_INTERVAL_SECONDS,
+  logLevel: logLevel,
+});
 
 const app = create(
-  new Logger(LOG_LEVEL),
-  MAX_AGE_IN_SECONDS,
+  logger,
+  parseInt(METRICS_APP_MAX_AGE_SECONDS, 10),
   getMonotonicTimeInSeconds,
-  (prune) => setInterval(prune, PRUNE_INTERVAL_SECONDS * 1000)
+  (prune) =>
+    setInterval(prune, parseInt(METRICS_APP_PRUNE_INTERVAL_SECONDS, 10) * 1000)
 );
-app.listen(PORT);
+app.listen(METRICS_APP_PORT);
