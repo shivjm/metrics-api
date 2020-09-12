0a050937b07add87fbad9f3e32978f8c25e13d76
Shiv Jha-Mathur <shivjm@users.noreply.github.com>
2020-09-12 22:36:59 +0530

    feat!: implement HTTP endpoints

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,17 @@
+import { Level, Logger } from "./logger";
 import { create } from "./server";
+import { getMonotonicTimeInSeconds } from "./time";
 
 // TODO read from environment
 const PORT = 3000;
+const MAX_AGE_IN_SECONDS = 60;
+const LOG_LEVEL = Level.TRACE;
+const PRUNE_INTERVAL_SECONDS = 1;
 
-const app = create();
+const app = create(
+  new Logger(LOG_LEVEL),
+  MAX_AGE_IN_SECONDS,
+  getMonotonicTimeInSeconds,
+  (prune) => setInterval(prune, PRUNE_INTERVAL_SECONDS * 1000)
+);
 app.listen(PORT);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,51 @@
 import * as express from "express";
 
-export function create() {
-    const app = express();
+import { Logger } from "./logger";
+import { Metrics } from "./metrics";
 
-    app.get("/", (_req, res) => {
-        res.send("Hello, World!");
+export function create(
+  logger: Logger,
+  maxAgeInSeconds: number,
+  getCurrentTime: () => number,
+  registerPruneCallback: (callback: () => void) => void
+) {
+  const app = express();
+  app.use(express.json());
+
+  const metrics = new Metrics(maxAgeInSeconds, getCurrentTime);
+  registerPruneCallback(() => metrics.prune());
+
+  app.post("/metric/:metric", (req, res) => {
+    const metric = req.params.metric;
+    const raw = req.body.value;
+    const value = parseInt(raw, 10);
+    logger.info("Request received: record", {
+      metric,
+      value,
     });
 
-    return app;
+    if (isNaN(value) || value === NaN) {
+      res.status(400).send("`value` must be a number");
+      return;
+    }
+
+    metrics.record(metric, req.body.value);
+
+    res.sendStatus(204);
+  });
+
+  app.get("/metric/:metric/sum", (req, res) => {
+    const metric = req.params.metric;
+    logger.info("Request received: sum", { metric });
+    const value = metrics.sum(metric);
+
+    if (value === undefined) {
+      res.status(400).send(`Unknown metric: ${metric}`);
+      return;
+    }
+
+    res.status(200).send(value.toString(10));
+  });
+
+  return app;
 }
